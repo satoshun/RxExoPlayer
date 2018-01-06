46cc3bb4fb967cb82afdf5d08898f81f8b29e56e
SatoShun <shun.sato1@gmail.com>
2018-01-06 14:16:37 +0000

    add new api events: create a all events stream

---
+++ exoplayer/src/main/java/com/github/satoshun/reactivex/exoplayer2/RxExoPlayerEventsObservable.kt
@@ -0,0 +1,81 @@
+package com.github.satoshun.reactivex.exoplayer2
+
+import com.google.android.exoplayer2.ExoPlaybackException
+import com.google.android.exoplayer2.PlaybackParameters
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.Timeline
+import com.google.android.exoplayer2.source.TrackGroupArray
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.android.MainThreadDisposable
+
+internal class RxExoPlayerEventsObservable(
+    private val player: Player
+) : Observable<RxExoPlayerEvent>() {
+  override fun subscribeActual(observer: Observer<in RxExoPlayerEvent>) {
+    val listener = Listener(observer, player)
+    observer.onSubscribe(listener)
+    player.addListener(listener)
+  }
+
+  private class Listener(
+      private val observer: Observer<in RxExoPlayerEvent>,
+      private val player: Player
+  ) : MainThreadDisposable(),
+      Player.EventListener {
+    override fun onPlaybackParametersChanged(playbackParameters: PlaybackParameters) {
+      if (isDisposed) return
+      observer.onNext(PlaybackParametersChangedEvent(playbackParameters))
+    }
+
+    override fun onSeekProcessed() {
+      if (isDisposed) return
+      observer.onNext(SeekProcessedEvent)
+    }
+
+    override fun onTracksChanged(trackGroups: TrackGroupArray, trackSelections: TrackSelectionArray) {
+      if (isDisposed) return
+      observer.onNext(TracksChangedEvent(trackGroups, trackSelections))
+    }
+
+    override fun onPlayerError(error: ExoPlaybackException) {
+      if (isDisposed) return
+      observer.onNext(PlayerErrorEvent(error))
+    }
+
+    override fun onLoadingChanged(isLoading: Boolean) {
+      if (isDisposed) return
+      observer.onNext(LoadingChangedEvent(isLoading))
+    }
+
+    override fun onPositionDiscontinuity(reason: Int) {
+      if (isDisposed) return
+      observer.onNext(PositionDiscontinuityEvent(reason))
+    }
+
+    override fun onRepeatModeChanged(repeatMode: Int) {
+      if (isDisposed) return
+      observer.onNext(RepeatModeChangedEvent(repeatMode))
+    }
+
+    override fun onShuffleModeEnabledChanged(shuffleModeEnabled: Boolean) {
+      if (isDisposed) return
+      observer.onNext(ShuffleModeEnabledChangedEvent(shuffleModeEnabled))
+    }
+
+    override fun onTimelineChanged(timeline: Timeline, manifest: Any) {
+      if (isDisposed) return
+      observer.onNext(TimelineChangedEvent(timeline, manifest))
+    }
+
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+      if (isDisposed) return
+      observer.onNext(PlayerStateChangedEvent(playWhenReady, playbackState))
+    }
+
+    override fun onDispose() {
+      player.removeListener(this)
+    }
+  }
+}
