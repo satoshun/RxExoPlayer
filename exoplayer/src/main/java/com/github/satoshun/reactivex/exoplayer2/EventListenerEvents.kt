4685d7436a0d22d5890af89ce218f19eb5beb5a1
SatoShun <shun.sato1@gmail.com>
2018-01-06 14:09:18 +0000

    add RxExoPlayerEvent

--- exoplayer/src/main/java/com/github/satoshun/reactivex/exoplayer2/EventListenerEvents.kt
+++ exoplayer/src/main/java/com/github/satoshun/reactivex/exoplayer2/EventListenerEvents.kt
@@ -6,43 +6,45 @@ import com.google.android.exoplayer2.Timeline
 import com.google.android.exoplayer2.source.TrackGroupArray
 import com.google.android.exoplayer2.trackselection.TrackSelectionArray
 
+sealed class RxExoPlayerEvent
+
 class PlaybackParametersChangedEvent(
     val playbackParameters: PlaybackParameters
-)
+) : RxExoPlayerEvent()
 
-object SeekProcessedEvent
+object SeekProcessedEvent : RxExoPlayerEvent()
 
 class TracksChangedEvent(
     val trackGroups: TrackGroupArray,
     val trackSelections: TrackSelectionArray
-)
+) : RxExoPlayerEvent()
 
 class PlayerErrorEvent(
     val error: ExoPlaybackException
-)
+) : RxExoPlayerEvent()
 
 class LoadingChangedEvent(
     val isLoading: Boolean
-)
+) : RxExoPlayerEvent()
 
 class PositionDiscontinuityEvent(
     val reason: Int
-)
+) : RxExoPlayerEvent()
 
 class RepeatModeChangedEvent(
     val repeatMode: Int
-)
+) : RxExoPlayerEvent()
 
 class ShuffleModeEnabledChangedEvent(
     val shuffleModeEnabled: Boolean
-)
+) : RxExoPlayerEvent()
 
 class TimelineChangedEvent(
     val timeline: Timeline,
     val manifest: Any
-)
+) : RxExoPlayerEvent()
 
 class PlayerStateChangedEvent(
     val playWhenReady: Boolean,
     val playbackState: Int
-)
+) : RxExoPlayerEvent()
