d804dd17f05f899c994dcf6942619469cb0de18b
SatoShun <shun.sato1@gmail.com>
2018-06-18 00:11:38 +0000

    remove unnecessary annotation and add CheckResult annotation

--- exoplayer/src/main/java/com/github/satoshun/reactivex/exoplayer2/RxExoPlayer.kt
+++ exoplayer/src/main/java/com/github/satoshun/reactivex/exoplayer2/RxExoPlayer.kt
@@ -1,7 +1,6 @@
 package com.github.satoshun.reactivex.exoplayer2
 
 import android.support.annotation.CheckResult
-import android.support.annotation.NonNull
 import com.github.satoshun.reactivex.exoplayer2.internal.LoadingChangedObservable
 import com.github.satoshun.reactivex.exoplayer2.internal.PlaybackParametersChangedObservable
 import com.github.satoshun.reactivex.exoplayer2.internal.PlayerErrorObservable
@@ -13,72 +12,60 @@ import com.github.satoshun.reactivex.exoplayer2.internal.SeekProcessedObservable
 import com.github.satoshun.reactivex.exoplayer2.internal.ShuffleModeEnabledChangedObservable
 import com.github.satoshun.reactivex.exoplayer2.internal.TimelineChangedObservable
 import com.github.satoshun.reactivex.exoplayer2.internal.TracksChangedObservable
-
 import com.google.android.exoplayer2.Player
 import io.reactivex.Observable
 
 object RxExoPlayer {
-  @NonNull
   @CheckResult
   fun events(player: Player): Observable<out RxExoPlayerEvent> {
     return RxExoPlayerEventsObservable(player)
   }
 
-  @NonNull
   @CheckResult
   fun playbackParametersChanged(player: Player): Observable<PlaybackParametersChangedEvent> {
     return PlaybackParametersChangedObservable(player)
   }
 
-  @NonNull
   @CheckResult
   fun seekProcessed(player: Player): Observable<SeekProcessedEvent> {
     return SeekProcessedObservable(player)
   }
 
-  @NonNull
   @CheckResult
   fun tracksChanged(player: Player): Observable<TracksChangedEvent> {
     return TracksChangedObservable(player)
   }
 
-  @NonNull
   @CheckResult
   fun playerError(player: Player): Observable<PlayerErrorEvent> {
     return PlayerErrorObservable(player)
   }
 
-  @NonNull
   @CheckResult
   fun loadingChanged(player: Player): Observable<LoadingChangedEvent> {
     return LoadingChangedObservable(player)
   }
 
-  @NonNull
   @CheckResult
   fun positionDiscontinuity(player: Player): Observable<PositionDiscontinuityEvent> {
     return PositionDiscontinuityObservable(player)
   }
 
-  @NonNull
   @CheckResult
   fun repeatModeChanged(player: Player): Observable<RepeatModeChangedEvent> {
     return RepeatModeChangedObservable(player)
   }
 
-  @NonNull
   @CheckResult
   fun shuffleModeEnabledChanged(player: Player): Observable<ShuffleModeEnabledChangedEvent> {
     return ShuffleModeEnabledChangedObservable(player)
   }
 
-  @NonNull
   @CheckResult
   fun timelineChanged(player: Player): Observable<TimelineChangedEvent> {
     return TimelineChangedObservable(player)
   }
 
-  @NonNull
   @CheckResult
   fun playerStateChanged(player: Player): Observable<PlayerStateChangedEvent> {
     return PlayerStateChangedObservable(player)
--- exoplayer/src/main/java/com/github/satoshun/reactivex/exoplayer2/hls/RxMediaSourceEvent.kt
+++ exoplayer/src/main/java/com/github/satoshun/reactivex/exoplayer2/hls/RxMediaSourceEvent.kt
@@ -1,9 +1,11 @@
 package com.github.satoshun.reactivex.exoplayer2.hls
 
+import android.support.annotation.CheckResult
 import com.github.satoshun.reactivex.exoplayer2.hls.internal.MediaSourceEventObservable
 import com.google.android.exoplayer2.source.MediaSource
 import io.reactivex.Observable
 
+@CheckResult
 fun MediaSource.events(): Observable<MediaSourceEvent> {
   return MediaSourceEventObservable(this)
 }
