27c82a10b436568cd97cabf610f589cc88182e54
SatoShun <shun.sato1@gmail.com>
2018-06-18 00:35:40 +0000

    add javadoc comment

--- exoplayer/src/main/java/com/github/satoshun/reactivex/exoplayer2/internal/PlayerEventsObservable.kt
+++ exoplayer/src/main/java/com/github/satoshun/reactivex/exoplayer2/internal/PlayerEventsObservable.kt
@@ -3,10 +3,10 @@ package com.github.satoshun.reactivex.exoplayer2.internal
 import com.github.satoshun.reactivex.exoplayer2.LoadingChangedEvent
 import com.github.satoshun.reactivex.exoplayer2.PlaybackParametersChangedEvent
 import com.github.satoshun.reactivex.exoplayer2.PlayerErrorEvent
+import com.github.satoshun.reactivex.exoplayer2.PlayerEvent
 import com.github.satoshun.reactivex.exoplayer2.PlayerStateChangedEvent
 import com.github.satoshun.reactivex.exoplayer2.PositionDiscontinuityEvent
 import com.github.satoshun.reactivex.exoplayer2.RepeatModeChangedEvent
-import com.github.satoshun.reactivex.exoplayer2.PlayerEvent
 import com.github.satoshun.reactivex.exoplayer2.SeekProcessedEvent
 import com.github.satoshun.reactivex.exoplayer2.ShuffleModeEnabledChangedEvent
 import com.github.satoshun.reactivex.exoplayer2.TimelineChangedEvent
@@ -21,7 +21,7 @@ import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.MainThreadDisposable
 
-internal class RxExoPlayerEventsObservable(
+internal class PlayerEventsObservable(
     private val player: Player
 ) : Observable<PlayerEvent>() {
   override fun subscribeActual(observer: Observer<in PlayerEvent>) {
